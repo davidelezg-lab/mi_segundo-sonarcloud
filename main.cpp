d6988b6d49b8741bc5c471d88ed3a3992a234d5f
delezg <david.elez@msmk.university>
2026-07-02 13:44:20 +0200

    Primer programa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-int suma(int a, int b){return a+b;}
-
 int main(){
-    std::cout << suma(2,3);
+
+	int a=10;
+int b=20;
+int c=30;
+int d=40;
+int e=50;
+
+    std::cout << a+b+c+d+e<<std::endl;
+
+return 0;
+
 }
